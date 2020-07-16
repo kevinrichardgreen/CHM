a5df7fcff7f9d319b9f74d9c5b936b8a0b4cb861
Kevin Green <kevin.richard.green@gmail.com>
2020-07-16 13:25:01 -0600

    Remove ViennaCL includes from modules that don't use it

--- src/modules/interp_met/WindNinja.cpp
+++ src/modules/interp_met/WindNinja.cpp
@@ -326,7 +326,7 @@ void WindNinja::run(mesh& domain)
 
                 // get the transfert function and associated wind component for the interpolated wind direction
 		if(L_avg == -1)
-                {		                 
+                {
 			W_transf = face->parameter("Ninja" + std::to_string(d));   // transfert function
 		}else
 		{
@@ -354,7 +354,7 @@ void WindNinja::run(mesh& domain)
 		double W_transf1 = 0.;
                 // get the transfert function and associated wind component for the interpolated wind direction
 		if(L_avg == -1)
-		{	
+		{
                 	W_transf1 = face->parameter("Ninja" + std::to_string(d1));   // transfert function
                 }else
 		{
@@ -366,7 +366,7 @@ void WindNinja::run(mesh& domain)
 
 		double W_transf2 = 0.;
 		if(L_avg == -1)
-		{	
+		{
                 	W_transf2 = face->parameter("Ninja" + std::to_string(d2));   // transfert function
                 }else
 		{
